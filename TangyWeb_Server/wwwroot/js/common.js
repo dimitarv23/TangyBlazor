df2a4a625d0c4f17eff597eca4283247fa241ac1
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-11 18:46:00 +0300

    Added Updates

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,25 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, "Success!", { timeout: 5000 });
+    }
+    if (type == "error") {
+        toastr.error(message, "Failure!", { timeout: 5000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type == "success") {
+        Swal.fire(
+            'Success!',
+            message,
+            'success'
+        )
+    }
+    if (type == "error") {
+        Swal.fire(
+            'Failure!',
+            message,
+            'error'
+        )
+    }
+}
