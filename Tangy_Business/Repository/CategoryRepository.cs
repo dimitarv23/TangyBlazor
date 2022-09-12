91d743a11b6d43ba098ca8d966da3c026d3d5624
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-12 12:00:34 +0300

    Added Database and more functionalities

---
+++ Tangy_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Business.Repository.IRepository;
+using Tangy_DataAccess;
+using Tangy_DataAccess.Data;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public CategoryDTO Create(CategoryDTO objDTO)
+        {
+            var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
+            obj.CreateDate = DateTime.Now;
+
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return _mapper.Map<Category, CategoryDTO>(obj);
+        }
+
+        public CategoryDTO Get(int id)
+        {
+            var obj = _db.Categories.FirstOrDefault(u => u.ID == id);
+
+            if (obj != null)
+            {
+                return _mapper.Map<Category, CategoryDTO>(obj);
+            }
+
+            return new CategoryDTO();
+        }
+
+        public IEnumerable<CategoryDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories);
+        }
+
+        public CategoryDTO Update(CategoryDTO objDTO)
+        {
+            var objFromDB = _db.Categories.FirstOrDefault(u => u.ID == objDTO.ID);
+
+            if (objFromDB != null)
+            {
+                objFromDB.Name = objDTO.Name;
+                _db.Categories.Update(objFromDB);
+                _db.SaveChanges();
+
+                return _mapper.Map<Category, CategoryDTO>(objFromDB);
+            }
+
+            return objDTO;
+        }
+
+        public int Delete(int id)
+        {
+            var obj = _db.Categories.FirstOrDefault(u => u.ID == id);
+
+            if (obj != null)
+            {
+                _db.Categories.Remove(obj);
+                return _db.SaveChanges();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Tangy_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO); //Post request
+        public CategoryDTO Update(CategoryDTO objDTO); //Put request
+        public int Delete(int id); //Delete request
+        public CategoryDTO Get(int id); //Get request (for 1 object)
+        public IEnumerable<CategoryDTO> GetAll(); //Get request (for all)
+    }
+}
