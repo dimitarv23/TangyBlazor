86d1a08bf09d6c0c82b1beb7c9fbc1bb0ba708e8
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-09-13 12:56:23 +0300

    Added create functionality

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Tangy_Models
     public class CategoryDTO
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
     }
 }
